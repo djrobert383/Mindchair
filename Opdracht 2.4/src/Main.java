d0457b0664658a900cf5796a87ff1fc5fdd3e91b
djrobert383 <djrobert@live.nl>
2014-02-11 15:18:01 +0100

    Optie menu gemaakt voor muziek

--- Opdracht 2.4/src/Main.java
+++ Opdracht 2.4/src/Main.java
@@ -1,12 +1,52 @@
-import java.io.File;
-import java.util.ArrayList;
-
 import lejos.nxt.Button;
-import lejos.nxt.Sound;
+import lejos.nxt.ButtonListener;
 
-public class Main {
+public class Main implements ButtonListener {
 	public static void main(String[] args){
-		Sound.setVolume(Sound.VOL_MAX);
-		Sound.playSample(new File("Geluid.wav"));
+		new Main();
+	}
+	
+	public boolean nothingSelected=true;
+	
+	public Main(){
+		options();
+	}
+	public void options(){
+		clearScreen();
+		System.out.println("Select Melody:");
+		selection(true, "Melody 1");
+		selection(false, "Melody 2");
+		Button.LEFT.addButtonListener(this);
+		Button.RIGHT.addButtonListener(this);
+	}
+	public void clearScreen(){
+		for(int i=0; i<7;i++){
+			System.out.println();
+		}
+	}
+	public void selection(boolean selected,String text){
+		clearScreen();
+		if(selected){
+			System.out.println(">"+text);
+		}else{
+			System.out.println(" "+text);			
+		}
+	}
+	@Override
+	public void buttonPressed(Button b) {
+		clearScreen();
+		System.out.println("Select Melody:");
+		if(b.equals(Button.LEFT)){
+			selection(true, "Melody 1");
+			selection(false, "Melody 2");
+		}
+		if(b.equals(Button.RIGHT)){
+			selection(false, "Melody 1");
+			selection(true, "Melody 2");
+		}
+	}
+	@Override
+	public void buttonReleased(Button b) {
+		
 	}
 }
