c232c0a9189d47f9e58a9418ee181f997e4e8c78
djrobert383 <djrobert@live.nl>
2014-02-11 13:59:29 +0100

    Voor jacob sound ding

---
+++ Opdracht 2.4/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+import lejos.nxt.Button;
+import lejos.nxt.Sound;
+
+public class Main {
+	public static void main(String[] args){
+		Sound.setVolume(Sound.VOL_MAX);
+		Sound.playSample(file);
+	}
+}
