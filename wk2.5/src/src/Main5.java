a08c6278903e99858692051817c77a1cd61dca9f
pimvanhespen <pimvanhespen@gmail.com>
2014-02-11 23:48:58 +0100

    opdracht 5 gefixed

--- wk2.5/src/src/Main5.java
+++ wk2.5/src/src/Main5.java
@@ -7,30 +7,52 @@ import lejos.nxt.NXTRegulatedMotor;
 public class Main5 {
 
 	public static void main(String[] arg) {
+
 		NXTRegulatedMotor motorA = Motor.getInstance(0);
 		NXTRegulatedMotor motorC = Motor.getInstance(2);
 
-		Motor.B.lock(180);
 		motorA.resetTachoCount();
 		motorC.resetTachoCount();
-		motorA.setAcceleration(150);
-		motorC.setAcceleration(150);
-		motorA.setSpeed(360);
-		motorC.setSpeed(360);
-		
+		motorA.setSpeed(180);
+		motorC.setSpeed(180);
 		motorA.forward();
 		motorC.forward();
-
+		System.out.println("1");
 		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException ie) {}
-
+			Thread.sleep(900);
+		} catch (InterruptedException ie) {
+		}
+		
+		motorA.setAcceleration(90);
+		motorC.setAcceleration(90);
+		motorA.setSpeed(360);
+		motorC.setSpeed(360);
+		System.out.println("2");
+		try {
+			Thread.sleep(900);
+		} catch (InterruptedException ie) {
+		}
+		
+		motorA.setAcceleration(180);
+		motorC.setAcceleration(180);
+		motorA.setSpeed(540);
+		motorC.setSpeed(540);
+		System.out.println("3");
+		try {
+			Thread.sleep(900);
+		} catch (InterruptedException ie) {
+		}
+		
+		System.out.println("4");
+		
 		motorA.stop();
 		motorC.stop();
-		motorA.rotateTo(1080);
-		motorC.rotateTo(1080);
+		System.out.println("5");
 		int tachoCountA = motorA.getTachoCount();
 		int tachoCountC = motorC.getTachoCount();
+
+		motorC.rotateTo(tachoCountA);
+		
 		System.out.println("count A = " + tachoCountA + " (should be 1080)");
 		System.out.println("count C = " + tachoCountC + " (should be 1080)");
 
