be43bfe21cefcf435cc67a691340327ed885a8b5
djrobert383 <djrobert@live.nl>
2014-02-25 11:15:02 +0100

    Allerlei setters toegevoegd

--- Opdracht 3.1/src/DriveCircle.java
+++ Opdracht 3.1/src/DriveCircle.java
@@ -4,7 +4,7 @@ public class DriveCircle {
 
 	private float wheelSpace;// afstand tussen de wielen in mm
 	private float straal; // in mm
-	private float tijd;//tijd om omwenteling te doen in seconden
+	private float tijd;// tijd om omwenteling te doen in seconden
 	private float buitenOmtrek;
 	private float binnenOmtrek;
 	private float buitenSnelheid;
@@ -13,12 +13,46 @@ public class DriveCircle {
 	public DriveCircle(float wheelSpace, float straal, float tijd) {
 		this.wheelSpace = wheelSpace;
 		this.straal = straal;
-		this.tijd=tijd;
-		buitenOmtrek = (float) (2* Math.PI * straal);
+		this.tijd = tijd;
+		berekenOmtrek();
+		berekenSnelheid();
+	}
+
+	public void setStraal(float straal) {
+		this.straal = straal;
+		berekenOmtrek();
+		berekenSnelheid();
+	}
+
+	public void setTijd(float tijd) {
+		this.tijd = tijd;
+		berekenOmtrek();
+		berekenSnelheid();
+	}
+
+	public void setAfstand(float afstand) {
+		buitenOmtrek = afstand;
+		binnenOmtrek = buitenOmtrek - buitenOmtrek;
+		berekenSnelheid();
+	}
+
+	public void setSnelheid(float snelheid) {
+		// t = s/v
+		tijd = snelheid / buitenOmtrek;
+		berekenOmtrek();
+		berekenSnelheid();
+	}
+
+	public void berekenOmtrek() {
+		buitenOmtrek = (float) (2 * Math.PI * straal);
 		binnenOmtrek = buitenOmtrek - wheelSpace;
-		//v = s/t
+	}
+
+	public void berekenSnelheid() {
+		// v = s/t
 		buitenSnelheid = buitenOmtrek / tijd;
 		binnenSnelheid = binnenOmtrek / tijd;
+
 	}
 
 	public void start() {
