85882792c8d471517233c4ede18fecb1f60674d6
pimvanhespen <pimvanhespen@gmail.com>
2014-03-04 17:15:43 +0100

    Week 3 opdracht 2

---
+++ Week 3 opdracht 2/src/Main.java
@@ -0,0 +1,31 @@
+import lejos.nxt.Motor;
+
+
+public class Main {
+
+	private static float WHEEL_DIAMETER = 3.6f;
+	private static float WHEEL_DISTANCE = 14.0f;	
+	
+	public Main(){
+		drive(10,100);
+	}
+
+	private void drive(float speed, float distance) {
+		
+		float wheel_circumference = WHEEL_DIAMETER * (float) Math.PI;
+		
+		float degrees_to_turn = (distance / wheel_circumference) * 360;
+		
+		float motor_speed = ( speed / wheel_circumference ) * 360;
+		
+		Motor.A.setSpeed(motor_speed);
+		Motor.B.setSpeed(motor_speed);
+		Motor.A.rotate((int) degrees_to_turn, true);
+		Motor.B.rotate((int) degrees_to_turn);
+				
+	}
+	
+	public static void main(String[] args) {
+		new Main();
+	}
+}
