6ef04b7042c811c907aadac63f99b9891a35ee40
pimvanhespen <pimvanhespen@gmail.com>
2014-03-04 16:50:21 +0100

    wek 3 opdracht 4

---
+++ week 3 opdracht 4/src/Main.java
@@ -0,0 +1,35 @@
+import lejos.nxt.Button;
+import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.util.Delay;
+
+
+public class Main {
+	
+	public static void main(String[] arg) throws InterruptedException{
+
+		NXTRegulatedMotor motorA = Motor.getInstance(0);
+		NXTRegulatedMotor motorC = Motor.getInstance(2);
+	
+		DifferentialPilot pilot = new DifferentialPilot(1.65f, 1.65f, motorA, motorC, true);
+		pilot.rotate(90);
+		pilot.travel(50,true);
+		Thread.sleep(500);
+		pilot.rotate(90);
+		pilot.travel(50,true);
+		Thread.sleep(500);
+		pilot.rotate(90);
+		pilot.travel(50,true);
+		Thread.sleep(500);
+		pilot.rotate(90);
+		pilot.travel(50,true);
+		Thread.sleep(500);
+		while(pilot.isMoving())Thread.yield();
+		pilot.stop();
+	
+	
+		Button.waitForAnyPress();
+		
+	}
+}
