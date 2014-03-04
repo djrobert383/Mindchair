20085086b3b83abf09a195ceeb78ca67678c1e87
djrobert383 <djrobert@live.nl>
2014-03-04 12:20:53 +0100

    constructor van main maakt nu een nieuwe main aan =p

--- Opdracht 2 en 4/src/Main.java
+++ Opdracht 2 en 4/src/Main.java
@@ -1,5 +1,4 @@
 import lejos.nxt.Button;
-import lejos.nxt.ColorSensor;
 import lejos.nxt.LCD;
 import lejos.nxt.LightSensor;
 import lejos.nxt.SensorPort;
@@ -7,6 +6,7 @@ import lejos.nxt.SensorPort;
 public class Main {
 	public ColorSensor cs;
 	public LightSensor ls;
+
 	public static void main(String[] args) {
 		new Main();
 	}
@@ -14,31 +14,33 @@ public class Main {
 	public Main() {
 		cs = new ColorSensor(SensorPort.S2);
 		ls = new LightSensor(SensorPort.S1);
+		LCD.clear();
 		System.out.println("Geef een witte ");
 		System.out.println("ondergrond");
-		
+
 		System.out.println("en druk op enter");
 		Button.ENTER.waitForPress();
 		cs.calibrateHigh();
 		ls.calibrateHigh();
 		LCD.clear();
-		System.out.println("color sensor waarde: "+cs.getHigh());
-		System.out.println("light sensor waarde: "+ls.getHigh());
+		System.out.println("color sensor waarde: " + cs.getHigh());
+		System.out.println("light sensor waarde: " + ls.getHigh());
 		System.out.println();
 		System.out.println("druk op enter om verder te gaan");
 		Button.ENTER.waitForPress();
-		
+		LCD.clear();
+
 		System.out.println("Geef een zwarte ondergrond");
 		System.out.println("en druk op enter");
 		Button.ENTER.waitForPress();
 		cs.calibrateLow();
 		ls.calibrateLow();
 		LCD.clear();
-		System.out.println("color sensor waarde: "+cs.getLow());
-		System.out.println("light sensor waarde: "+ls.getLow());
+		System.out.println("color sensor waarde: " + cs.getLow());
+		System.out.println("light sensor waarde: " + ls.getLow());
 		System.out.println();
 		System.out.println("druk op enter om verder te gaan");
 		Button.ENTER.waitForPress();
-		
+		new Main();
 	}
 }
