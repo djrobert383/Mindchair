d826d5b80c14f5b869f12ac42885623049a03968
djrobert383 <djrobert@live.nl>
2014-03-28 16:11:15 +0100

    dingetjes toegevoegd

--- EindProject/src/nxt/Controller.java
+++ EindProject/src/nxt/Controller.java
@@ -1,7 +1,29 @@
 package nxt;
 
-public class Controller extends Thread{
-	public void run(){
+public class Controller extends Thread implements LeftSensorListener,
+		RightSensorListener, UltraSonicSensorListener {
+	public void run() {
+
+	}
+
+	@Override
+	public void ultraSonicChanged(UpdatingSensor us, float oldValue,
+			float newValue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rightSensorChanged(UpdatingSensor updatingSensor,
+			float oldValue, float newValue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void leftSensorChanged(UpdatingSensor updatingSensor,
+			float oldValue, float newValue) {
+		// TODO Auto-generated method stub
 		
 	}
 }
