bc460f36bd76ebda9aa61285cf17f44c2f329e7a
pimvanhespen <pimvanhespen@gmail.com>
2014-04-01 13:20:06 +0200

    Controller aangepast

--- EindProject/src/nxt/ObstructionController.java
+++ EindProject/src/nxt/ObstructionController.java
@@ -2,17 +2,36 @@ package nxt;
 
 public class ObstructionController extends Thread implements LightSensorListener, UltraSonicSensorListener{
 	
+	private int current_distance = 255;
 	
+	private final int SAFE_DISTANCE = 30;
 	
-	public ObstructionController(ColorSensor cs, LightSensor ls,
-			UltraSonicSensor us) {
+	public ObstructionController(ColorSensor cs, LightSensor ls, UltraSonicSensor us) {
 		cs.addListener(this);
 		ls.addListener(this);
 		us.addListener(this);
 	}
 	
 	public void run(){
-		
+		while(true){
+			if(current_distance < SAFE_DISTANCE ){
+				try{
+					MotorController.stop();
+					MotorController.turnOnPlace(90);
+					MotorController.driveForward();
+					Thread.sleep(1500);
+					MotorController.stop();
+					MotorController.turnOnPlace(-90);
+					MotorController.driveForward();
+					Thread.sleep(1500);
+					MotorController.stop();
+					MotorController.turnOnPlace(-90);
+					MotorController.driveForward();
+					Thread.sleep(1500);
+					MotorController.turnOnPlace(90);
+				} catch (InterruptedException IE){}
+			}
+		}
 	}
 
 	@Override
