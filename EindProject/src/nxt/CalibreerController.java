c15fe030ba6673f89daec3c75b476089cc17bc3f
Ban-aan <Becommenteerd door Ban-aan. Sms ban-aan naar 6464.>
2014-04-01 15:27:41 +0200

    Calibreer done

--- EindProject/src/nxt/CalibreerController.java
+++ EindProject/src/nxt/CalibreerController.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 public class CalibreerController {
 
-	private int _high;
-	private int _low;
 	private ArrayList<UpdatingSensor> sensors;
 
 	public CalibreerController(ArrayList<UpdatingSensor> sensors) {
@@ -13,11 +11,29 @@ public class CalibreerController {
 	}
 
 	public void calibreer() {
+		int meetWaarde;
 		for (int graden = 0; graden < 360; graden++) {
 			MotorController.turnOnPlace(1);
 			for(UpdatingSensor sensor : sensors){
 				if(sensor.getSensorType() == SensorType.Colorsensor){
-					((ColorSensor) sensor).getNormalizedLightValue();
+					ColorSensor tmp = (ColorSensor) sensor;
+					meetWaarde = tmp.getRawLightValue();
+					if (meetWaarde < tmp.getLow()){
+						tmp.setLow(meetWaarde);
+					}
+					else if (meetWaarde > tmp.getHigh()) {
+						tmp.setHigh(meetWaarde);					
+					}
+				}
+				if(sensor.getSensorType() == SensorType.Lightsensor){
+					LightSensor tmp = (LightSensor) sensor;
+					meetWaarde = tmp.getLightValue();
+					if (meetWaarde < tmp.getLow()){
+						tmp.setLow(meetWaarde);
+					}
+					else if (meetWaarde > tmp.getHigh()) {
+						tmp.setHigh(meetWaarde);					
+					}
 				}
 			}
 		}
